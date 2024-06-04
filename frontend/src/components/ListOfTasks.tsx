466b0ccaef6ac93d430074431aa26557638cec54
rodrothbarth <rothbarth@logikee.com.br>
2024-06-04 01:47:14 -0300

    main - update edit/list components rendering swap

--- frontend/src/components/ListOfTasks.tsx
+++ frontend/src/components/ListOfTasks.tsx
@@ -33,24 +33,25 @@ const {setTasks, tasks} = useTask()
     return(
         <>
             <h1>Task Management App</h1>
-                 {isEdit && (
+                 {isEdit ?  (
                      <TaskForm onFinishEdit={()=>setIsEdit(false)} editTask={id}/>
-                 )}
-                    {tasks!.map(task => (
-                        <div key={task.id} style={{border:"2px solid #ffffff", marginTop: "10px"}}>
-                                <div style={{
-                                    display:"flex",
-                                    alignItems:"center",
-                                    gap: "10px",
-                                    width: "5em",
-                                    minWidth:"100%",
-                                }}>
-                                    <h3 style={{marginLeft:"5%", width:"25%"}}>{task.title}</h3>
-                                    <p style={{ width:"35%"}}>{task.description}</p>
-                                    <button style={{height:"3rem", backgroundColor:"red"}} onClick={() => deleteTask(task.id)}>Delete</button>
-                                    <button style={{height:"3rem", backgroundColor:"blue"}} onClick={() => editTask(task)}>Edit</button>
-                                </div>
-                        </div>
-                    ))}
+                 ): (<>
+                     {tasks!.map(task => (
+                         <div key={task.id} style={{border:"2px solid #ffffff", marginTop: "10px"}}>
+                             <div style={{
+                                 display:"flex",
+                                 alignItems:"center",
+                                 gap: "10px",
+                                 width: "5em",
+                                 minWidth:"100%",
+                             }}>
+                                 <h3 style={{marginLeft:"5%", width:"25%"}}>{task.title}</h3>
+                                 <p style={{ width:"35%"}}>{task.description}</p>
+                                 <button style={{height:"3rem", backgroundColor:"red"}} onClick={() => deleteTask(task.id)}>Delete</button>
+                                 <button style={{height:"3rem", backgroundColor:"blue"}} onClick={() => editTask(task)}>Edit</button>
+                             </div>
+                         </div>
+                     ))}</>)}
+
     </>)
 }
